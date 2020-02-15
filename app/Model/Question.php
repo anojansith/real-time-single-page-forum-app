deeb08f0a831ebb4f25fc61ae17f6f7807894d0d
Anojan Sithamparappillai <sithanojan@yahoo.com>
2020-02-15 14:46:04 -0500

    Relationship

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Model;
+namespace App\User;
 
+use App\Model\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
