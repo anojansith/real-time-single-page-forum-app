fdc013f1343908520752115fea1dcac9e5d08e95
Anojan Sithamparappillai <sithanojan@yahoo.com>
2020-04-04 01:19:58 -0400

    JWT Middleware and Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,11 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +55,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof TokenBlacklistedException){
+            return response(['error' => 'Token cannot be used, get a new one'],Response::HTTP_BAD_REQUEST);
+        }
+        else if($exception instanceof TokenInvalidException){
+            return response(['error' => 'Token is invalid'], Response::HTTP_BAD_REQUEST);
+        }
+
+        else if($exception instanceof TokenExpiredException){
+            return response(['error' => 'Token is expired'], Response::HTTP_BAD_REQUEST);
+        }
+
+        else if($exception instanceof JWTException){
+            return response(['error' => 'Token is not provided'], 500);
+        }
+
+
         return parent::render($request, $exception);
     }
 }
